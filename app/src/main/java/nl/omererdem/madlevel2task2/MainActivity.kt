f61fe6ccef5acc717afd54f3a5bb19906bea7373
Omer Erdem <m.o.erdem@outlook.com>
2020-09-14 23:25:01 +0200

    #3.2 Swipe listener

--- app/src/main/java/nl/omererdem/madlevel2task2/MainActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel2task2/MainActivity.kt
@@ -2,8 +2,10 @@ package nl.omererdem.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import nl.omererdem.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +25,33 @@ class MainActivity : AppCompatActivity() {
     fun initViews() {
         binding.rvQuestions.layoutManager = LinearLayoutManager(this)
         binding.rvQuestions.adapter = questionAdapter
-        Log.i("questionlist", questions.toString())
         questionAdapter.notifyDataSetChanged()
+        createItemTouchHelper().attachToRecyclerView(rvQuestions)
     }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                questions[position].result = true
+                questionAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
 }
