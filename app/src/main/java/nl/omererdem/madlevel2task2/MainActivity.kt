f7a9a9aa389fbb6547f59976739d2740b2b6a883
Omer Erdem <m.o.erdem@outlook.com>
2020-09-14 11:48:00 +0200

    #3.1 Show questions

--- app/src/main/java/nl/omererdem/madlevel2task2/MainActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel2task2/MainActivity.kt
@@ -2,10 +2,16 @@ package nl.omererdem.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import nl.omererdem.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val questions: List<Question> = Question.questions
+
+    private val questionAdapter = QuestionAdapter(questions)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initViews() {
-
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this)
+        binding.rvQuestions.adapter = questionAdapter
+        Log.i("questionlist", questions.toString())
+        questionAdapter.notifyDataSetChanged()
     }
 }
